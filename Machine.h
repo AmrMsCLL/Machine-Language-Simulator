3f4e06a6d236b52bb09f58e91c3ee2386ecabbc9
Ahmed Saber <146995775+AHmEeD-SaBER@users.noreply.github.com>
2023-11-11 23:44:32 +0200

    Update Machine.h

--- Machine.h
+++ Machine.h
@@ -1,7 +1,3 @@
-//
-// Created by ahmad on 11/10/2023.
-//
-
 #ifndef VOLE_MACHINE_MACHINE_H
 #define VOLE_MACHINE_MACHINE_H
 #include "Register.h"
@@ -13,13 +9,28 @@ private:
     int proCounter;
     Register r;
     Memory m;
+    string op;
+    string regster;
+    string value1;
+    string value2;
 
 public:
-    friend void converter(Register re);
     void loadfile(string filename);
+    void run();
     void decode();
     void execute();
-    friend string hexToint(string hexString);
+    friend int hexToint(string hexString);
+
+    void Op_code (string convertAdd);
+    void converter(string address);
+    void store(string address);
+    void loadFromemo(string regster, string address);
+    void addfloat(string address);
+    void load(string regster, string value);
+    void move(string address);
+    void add(string address);
+    void jump(string address);
+    void halt(string address);
 };
 
 
